71b3d26370d748d6e3c92d96db8962dbf44b15b5
Samuel Lorétan <tynril@gmail.com>
2013-11-18 17:55:37 +0100

    Waiting for key input only on Windows now.

--- test/test-main.cc
+++ test/test-main.cc
@@ -5,7 +5,9 @@ int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	int testsResult = RUN_ALL_TESTS();
 
+#ifdef _WIN32
 	std::cout << "Press Enter to continue..." << std::endl;
 	std::cin.get();
+#endif
 	return testsResult;
 }
